9363d0828aed586728feb2e63156b89fd17932c6
B3n30 <benediktthomas@gmail.com>
2017-08-16 11:15:14 +0200

    travis6

--- .travis-upload.sh
+++ .travis-upload.sh
@@ -3,7 +3,7 @@
 mkdir system_archives
 mkdir system_archives/shared_font
 
-mv shared_font/shared_font.app.romfs.h artifacts/shared_font/
-mv shared_font/00000000.app.romfs artifacts/shared_font/
+mv shared_font/shared_font.app.romfs.h system_archives/shared_font/
+mv shared_font/00000000.app.romfs system_archives/shared_font/
 
 7z a "system_archives.7z" system_archives
