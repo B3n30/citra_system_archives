f18379c3213430695778ee4f4a09cfa34328dbc8
B3n30 <benediktthomas@gmail.com>
2017-08-16 11:06:31 +0200

    travis5

--- .travis-upload.sh
+++ .travis-upload.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 
-mkdir artifacts
-mkdir artifacts/shared_font
+mkdir system_archives
+mkdir system_archives/shared_font
 
 mv shared_font/shared_font.app.romfs.h artifacts/shared_font/
 mv shared_font/00000000.app.romfs artifacts/shared_font/
 
-cd artifacts
-
-7z a "system_archives.7z" *
+7z a "system_archives.7z" system_archives
