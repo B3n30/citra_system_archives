d8c37697c374f66bb7a7c2267caddb75481256b9
B3n30 <benediktthomas@gmail.com>
2017-08-30 09:06:36 +0200

    Added some comments and Readme

--- shared_font/run.sh
+++ shared_font/run.sh
@@ -7,14 +7,24 @@ cmake -DUSE_DEP=OFF ..
 make
 cd ../..
 cp ignore_3dstool.txt 3dstool/bin/Release/
+
+# Create the images
 python create_png.py
+
+# Create the bcfnt file
 python bcfnt.py -cf code.bcfnt
+
+# Convert the bcfnt file into a romfs
 mkdir romfs
 ./3dstool/bin/Release/3dstool -zvf code.bcfnt --compress-type lzex --compress-out romfs/cbf_std.bcfnt.lz
 ./3dstool/bin/Release/3dstool -cvtf romfs romfs.bin --romfs-dir romfs
 dd bs=4096 skip=1 if=romfs.bin of=SHARED_FONT_DATA
+
+# Create a header file to include in citra
 xxd -i SHARED_FONT_DATA > shared_font.app.romfs.h
 mv SHARED_FONT_DATA 00000000.app.romfs
+
+# Cleanups
 rm -rf 3dstool
 rm -rf romfs
 rm code.bcfnt
