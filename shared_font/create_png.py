05a5ec7f85629dd3c284d1100d3614c5f5183707
B3n30 <benediktthomas@gmail.com>
2017-09-03 21:55:12 +0200

    Added position shift for m

--- shared_font/create_png.py
+++ shared_font/create_png.py
@@ -10,6 +10,8 @@ with open('code_manifest.json','r') as f:
     content = json.loads(f.read().replace('\n', ' '))['glyphMap']
     chars= {y:x for x,y in content.iteritems()}
 
+specific_offsets={"m":[-2,0]}
+
 rows=1
 cols=5
 sheetCount = 1501
@@ -30,7 +32,10 @@ for sheet in range(sheetCount):
         pos_x = x_start_offset
         for col in range(cols):
             if (current_char in chars.keys()):
-                draw.text((pos_x, pos_y), chars[current_char], "#FFF", font=font)
+                if (chars[current_char] in specific_offsets):
+                    draw.text((pos_x+specific_offsets[chars[current_char]][0], pos_y+specific_offsets[chars[current_char]][1]), chars[current_char], "#FFF", font=font)
+                else:
+                    draw.text((pos_x, pos_y), chars[current_char], "#FFF", font=font)
             current_char += 1
             if (current_char > 94):
                 font = PIL.ImageFont.truetype("unifont-10.0.05.ttf", 22)
