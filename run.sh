2d981191f0b3c7209684b47e21b8c3929a5b4321
B3n30 <benediktthomas@gmail.com>
2018-07-12 18:23:32 +0200

    Add bad word list generator

--- run.sh
+++ run.sh
@@ -1,8 +1,25 @@
 #!/bin/sh
 
+# Build 3dstool
+git clone --recursive https://github.com/dnasdw/3dstool
+mkdir 3dstool/build
+cd 3dstool/build
+cmake -DUSE_DEP=OFF ..
+make
+cd ../..
+cp ignore_3dstool.txt 3dstool/bin/Release/
+
 # Create the Shared Font
 cd shared_font
 ./run.sh
 cd ..
 
+# Create the Bad Word List
+cd bad_word_list
+./run.sh
+cd ..
+
 # TODO(B3N30): Create the other system archives
+
+# Cleanup
+#rm -rf 3dstool
--- shared_font/run.sh
+++ shared_font/run.sh
@@ -1,13 +1,5 @@
 #!/bin/sh
 
-git clone --recursive https://github.com/dnasdw/3dstool
-mkdir 3dstool/build
-cd 3dstool/build
-cmake -DUSE_DEP=OFF ..
-make
-cd ../..
-cp ignore_3dstool.txt 3dstool/bin/Release/
-
 # Create the images
 python create_png.py
 
@@ -19,19 +11,18 @@ cp reserved_unicode_chars/* .
 
 # Convert the bcfnt file into a romfs
 mkdir romfs
-./3dstool/bin/Release/3dstool -zvf code.bcfnt --compress-type lzex --compress-out romfs/cbf_std.bcfnt.lz
-./3dstool/bin/Release/3dstool -cvtf romfs romfs.bin --romfs-dir romfs
+./../3dstool/bin/Release/3dstool -zvf code.bcfnt --compress-type lzex --compress-out romfs/cbf_std.bcfnt.lz
+./../3dstool/bin/Release/3dstool -cvtf romfs romfs.bin --romfs-dir romfs
 dd bs=4096 skip=1 if=romfs.bin of=SHARED_FONT_DATA
 
 # Create a header file to include in citra
 echo -n "// Git Hash: " > shared_font.app.romfs.h
-git git rev-parse HEAD >> shared_font.app.romfs.h
+git rev-parse HEAD >> shared_font.app.romfs.h
 echo "" >> shared_font.app.romfs.h
 xxd -i SHARED_FONT_DATA >> shared_font.app.romfs.h
 mv SHARED_FONT_DATA 00000000.app.romfs
 
 # Cleanups
-rm -rf 3dstool
 rm -rf romfs
 rm code.bcfnt
 rm code_sheet*
